0dd7592f5968db52e5a26acfacfd0002dd599b87
DrogKris <khrystynkavishchuk@gmail.com>
2024-02-06 17:20:19 +0200

    Initial commit

---
+++ frontend/src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyC-41P0wKZyfAiGyu0IU24kd0bxLoNvWdM",
+    authDomain: "fire-operchat.firebaseapp.com",
+    projectId: "fire-operchat",
+    storageBucket: "fire-operchat.appspot.com",
+    messagingSenderId: "270444812101",
+    appId: "1:270444812101:web:d94e2ddb2133f0a692902c"
+})
